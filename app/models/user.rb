567ada8c05de0c7434d6ae946360178d4df50dee
Gerard Chin <gerardchin91@gmail.com>
2015-03-04 23:33:21 +1300

    Add email and name validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true,
+                   length: { minimum: 2,
+                             maximum: 50
+                           }
+
+  validates :email, presence: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    length: { minimum: 4,
+                              maximum: 255}
 end
