684375f8c0c4bbe589f3d6ad8c55deab70320aa6
Gerard Chin <gerardchin91@gmail.com>
2015-03-04 23:52:58 +1300

    Add callback to downcase email before saving to database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
   validates :name, presence: true,
                    length: { minimum: 2,
                              maximum: 50
