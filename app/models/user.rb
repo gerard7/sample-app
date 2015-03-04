cde2d6e09cf8116cc1bc0324252f69777b513823
Gerard Chin <gerardchin91@gmail.com>
2015-03-04 23:42:53 +1300

    Write code for and add uniqueness validation to email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ActiveRecord::Base
                    length: { minimum: 2,
                              maximum: 50
                            }
-
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX },
                     length: { minimum: 4,
                               maximum: 255}
 end
